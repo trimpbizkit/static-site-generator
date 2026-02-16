92ae1644ef622c1b39d52d1cfb96804147e2e46b
trimpbizkit <mgilmour.se@gmail.com>
2026-02-16 17:19:40 -0500

    add function to identify block type

--- src/block_functions.py
+++ src/block_functions.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown) -> list[str]:
     '''
     Split the given markdown document into a list of blocks
@@ -13,3 +24,44 @@ def markdown_to_blocks(markdown) -> list[str]:
             continue
         blocks.append(section.strip())
     return blocks
+
+def block_to_block_type(block) -> BlockType:
+    '''
+    Identify the type of the markdown block
+    
+    :param block: a single block of markdown text, assume all leading and trailing whitespace was stripped
+    :return: the enum representing the identified markdown block type
+    :rtype: BlockType
+    '''
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        # Headings start with 1-6 # characters, followed by a space and then the heading text.
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        # Multiline Code blocks must start with 3 backticks and a newline, then end with 3 backticks.
+        return BlockType.CODE
+    if block.startswith(">"):
+        # Every line in a quote block must start with a "greater-than" character: > followed by the quote text. 
+        # A space after > is allowed but not required.
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith('- '):
+        # Every line in an unordered list block must start with a - character, followed by a space.
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith('1. '):
+        # Every line in an ordered list block must start with a number followed by a . character and a space.
+        # The number must start at 1 and increment by 1 for each line.
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    # If none of the above conditions are met, the block is a normal paragraph.
+    return BlockType.PARAGRAPH
--- src/test_block_functions.py
+++ src/test_block_functions.py
@@ -1,7 +1,9 @@
 import unittest
 
 from block_functions import (
-    markdown_to_blocks
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType
 )
 
 class TestBlockFunctions(unittest.TestCase):
@@ -47,6 +49,51 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_paragraph_block(self):
+        md = "A paragraph block with some **bold** text"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.PARAGRAPH, type)
+
+    def test_heading_block(self):
+        md = "# This is a heading"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, type)
+        md = "## This is a heading"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, type)
+        md = "### This is a heading"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, type)
+        md = "#### This is a heading"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, type)
+        md = "##### This is a heading"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, type)
+        md = "###### This is a heading"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, type)
+        
+    def test_code_block(self):
+        md = "```\nThis is a code block\n```"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.CODE, type)
+    
+    def test_quote_block(self):
+        md = "> This is a quote block\n> Luke, I am your father\n> We are going to need a bigger boat"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.QUOTE, type)
+
+    def test_unordered_list_block(self):
+        md = "- This is an unordered list\n- with\n- items"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.ULIST, type)
+
+    def test_ordered_list_block(self):
+        md = "1. This is an ordered list\n2. with\n3. items"
+        type = block_to_block_type(md)
+        self.assertEqual(BlockType.OLIST, type)
 
 
 if __name__ == "__main__":
